b817af5a5be29f6872f8a6ba911aa3b6198d48e7
alvin <alvinsoh10@hotmail.com>
2024-04-03 20:06:22 +0800

    changed redis version

--- elasticache.tf
+++ elasticache.tf
@@ -3,7 +3,7 @@ resource "aws_elasticache_replication_group" "elasticache-tf" {
   description = "testing cache"
   node_type = "cache.t3.small"
   port = 6379
-  parameter_group_name = "default.redis3.2.cluster.on"
+  parameter_group_name = "default.redis7.cluster.on"
   automatic_failover_enabled = true
   num_node_groups = 2
   replicas_per_node_group = 1
