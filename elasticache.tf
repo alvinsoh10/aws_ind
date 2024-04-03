3e3dc4a2af98725d68cf5a9da2f45e0002fdebf8
alvin <alvinsoh10@hotmail.com>
2024-04-03 20:12:55 +0800

    added subnet group

--- elasticache.tf
+++ elasticache.tf
@@ -3,6 +3,7 @@ resource "aws_elasticache_replication_group" "elasticache-tf" {
   description = "testing cache"
   node_type = "cache.t3.small"
   port = 6379
+  subnet_group_name = aws_elasticache_subnet_group.elasticache-subnet-group
   parameter_group_name = "default.redis7.cluster.on"
   automatic_failover_enabled = true
   num_node_groups = 2
