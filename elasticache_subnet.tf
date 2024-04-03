ac339a3797dea03034b87af1ae88088af71457b2
alvin <alvinsoh10@hotmail.com>
2024-04-03 16:45:19 +0800

    added provider and elasticache subnet files

---
+++ elasticache_subnet.tf
@@ -0,0 +1,31 @@
+resource "aws_subnet" "subnet1" {
+    vpc_id = "vpc-007f4d7696454ac10"
+    cidr_block = "10.0.0.32/28"
+    availability_zone = "ap-southeast-1a"
+    tags = {
+        Name = "test-elasticache"
+    }
+}
+
+resource "aws_subnet" "subnet2" {
+    vpc_id = "vpc-007f4d7696454ac10"
+    cidr_block = "10.0.0.48/28"
+    availability_zone = "ap-southeast-1a"
+    tags = {
+        Name = "test-elasticache"
+    }
+}
+
+resource "aws_subnet" "subnet3" {
+    vpc_id = "vpc-007f4d7696454ac10"
+    cidr_block = "10.0.0.64/28"
+    availability_zone = "ap-southeast-1a"
+    tags = {
+        Name = "test-elasticache"
+    }
+}
+
+resource "aws_elasticache_subnet_group" "elasticache-subnet-group" {
+  name = "elasticache-subnet-group"
+  subnet_ids = [aws_subnet.subnet1, aws_subnet.subnet2, aws_subnet.subnet3]
+}
