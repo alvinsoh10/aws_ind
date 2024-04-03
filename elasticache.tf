d43a5d27075b7be55ebd4e6343bf738114c81043
alvin <alvinsoh10@hotmail.com>
2024-04-03 17:45:18 +0800

    push elasticache to the cloud

---
+++ elasticache.tf
@@ -0,0 +1,10 @@
+resource "aws_elasticache_replication_group" "elasticache-tf" {
+  replication_group_id = "tf-redis-test"
+  description = "testing cache"
+  node_type = "cache.t3.small"
+  port = 6379
+  parameter_group_name = "default.redis3.2.cluster.on"
+  automatic_failover_enabled = true
+  num_node_groups = 2
+  replicas_per_node_group = 1
+}
