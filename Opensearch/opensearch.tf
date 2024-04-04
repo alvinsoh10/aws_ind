002d57e69da953344fdb08b946bec6236fd0be9c
alvin <alvinsoh10@hotmail.com>
2024-04-04 15:43:46 +0800

    typo

--- Opensearch/opensearch.tf
+++ Opensearch/opensearch.tf
@@ -3,7 +3,7 @@ resource "aws_opensearch_domain" "opensearch_tf" {
   engine_version = "Elasticsearch_7.0"
 
   cluster_config {
-    instance_type = t3.small.search
+    instance_type = "t3.small.search"
   }
 
   tags = {
