87fd60530d87887ffe4baff9c2148183538143ab
alvin <alvinsoh10@hotmail.com>
2024-04-05 01:16:03 +0800

    added zone awareness

--- Opensearch_VPC/opensearch.tf
+++ Opensearch_VPC/opensearch.tf
@@ -3,6 +3,10 @@ resource "aws_opensearch_domain" "test-vpc-os" {
   engine_version = "OpenSearch_2.11"
   cluster_config {
     instance_type = "t3.small.search"
+    zone_awareness_config {
+      availability_zone_count = 2
+    }
+    zone_awareness_enabled = true
   }
   vpc_options {
     subnet_ids = [
@@ -11,6 +15,7 @@ resource "aws_opensearch_domain" "test-vpc-os" {
     ]
     security_group_ids = ["sg-0487554c2b69bf6b7"]
   }
+  
   ebs_options {
     volume_size = 10
     volume_type = "gp3"
