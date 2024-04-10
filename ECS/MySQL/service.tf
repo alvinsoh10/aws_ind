f7eea56a7d7ec45f5850b89214f19be85ec5978b
alvinswx <alvinswx@maxis.com.my>
2024-04-10 23:11:42 +0800

    removed placement strategy from service.tf

--- ECS/MySQL/service.tf
+++ ECS/MySQL/service.tf
@@ -5,11 +5,6 @@ resource "aws_ecs_service" "service_mysql" {
   desired_count = 1
   launch_type = "FARGATE"
 
-  ordered_placement_strategy {
-    type = "binpack"
-    field = "cpu"
-  }
-
   network_configuration {
     subnets = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
     assign_public_ip = true
