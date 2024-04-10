faa6ef52112b34021aaecc4952a16b95c3cd94a9
alvinswx <alvinswx@maxis.com.my>
2024-04-10 15:30:21 +0800

    added provider and task definition

---
+++ ECS/MySQL/task_definition.tf
@@ -0,0 +1,26 @@
+resource "aws_ecs_task_definition" "name" {
+  family = "service"
+  
+  container_definitions = jsonencode({
+    name = "first"
+    image = "mysql"
+    cpu = 1
+    memory = 2048
+    essential = true
+    portMappings = [
+        {
+            containerPort = 3306
+            hostPort = 3306
+        }
+    ]
+  })
+
+  volume {
+    name = "service-storage"
+    host_path = "/ecs/service-storage"
+  }
+   placement_constraints {
+     type = "memberOf"
+     expression = "attribute:ecs.availability-zone in [ap-southeast-1a, ap-southeast-1b]"
+   }
+}
