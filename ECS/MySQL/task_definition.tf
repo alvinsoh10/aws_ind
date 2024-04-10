4ca40bf12c52decab67d206b02c1c3d53f263c00
alvinswx <alvinswx@maxis.com.my>
2024-04-11 02:12:51 +0800

    create group in log configuration

--- ECS/MySQL/task_definition.tf
+++ ECS/MySQL/task_definition.tf
@@ -26,6 +26,7 @@ resource "aws_ecs_task_definition" "task_mysql" {
     log_configuration = {
       logDriver = "awslogs"
       options = {
+        "awslogs-create-group" = "true"
         "awslogs-group" = "fargate-logs-test"
         "awslogs-region" = "ap-southeast-1"
         "awslogs-stream-prefix" = "ecs"
