d67cb987c39c5b2b96efc1f4c102181cbd50a8d6
alvinswx <alvinswx@maxis.com.my>
2024-04-10 22:16:33 +0800

    added id and arn

--- ECS/MySQL/service.tf
+++ ECS/MySQL/service.tf
@@ -1,7 +1,7 @@
 resource "aws_ecs_service" "service_mysql" {
   name = "service_mysql"
-  cluster = aws_ecs_cluster.cluster_mysql
-  task_definition = aws_ecs_task_definition.task_mysql
+  cluster = aws_ecs_cluster.cluster_mysql.id
+  task_definition = aws_ecs_task_definition.task_mysql.arn
   desired_count = 1
   launch_type = "FARGATE"
 
