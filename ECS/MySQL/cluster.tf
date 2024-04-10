860f555e37e5bacd482f933daf4963827bc0b80f
alvinswx <alvinswx@maxis.com.my>
2024-04-10 23:58:02 +0800

    added logging at cluster level

--- ECS/MySQL/cluster.tf
+++ ECS/MySQL/cluster.tf
@@ -1,3 +1,14 @@
+resource "aws_cloudwatch_log_group" "example" {
+  name = "example"
+}
 resource "aws_ecs_cluster" "cluster_mysql" {
   name = "cluster_mysql"
+  configuration {
+    execute_command_configuration {
+      log_configuration {
+        cloud_watch_encryption_enabled = true
+        cloud_watch_log_group_name = aws_cloudwatch_log_group.example.name
+      }
+    }
+  }
 }
