cb79641daa7c24a48b60614dfaf34a4089cc9670
alvinswx <alvinswx@maxis.com.my>
2024-04-11 15:23:23 +0800

    equals to colon

--- ECS/MySQL/task_definition.tf
+++ ECS/MySQL/task_definition.tf
@@ -6,35 +6,36 @@ resource "aws_ecs_task_definition" "task_mysql" {
   network_mode = "awsvpc"
   task_role_arn = "arn:aws:iam::240835895323:role/ecsTaskExecutionRole"
   execution_role_arn = "arn:aws:iam::240835895323:role/ecsTaskExecutionRole"
-  container_definitions = <<DEFINITION
-[
+
+  container_definitions = <<TASK_DEFINITION
+  [
     {
-      "name" : "first",
-      "image" : "mysql",
-      "essential" : "true",
-      "portMappings" : [
+        "name" : "first",
+        "image" : "mysql",
+        "essential" : "true",
+        "portMappings" : [
+            {
+                "containerPort" : "3306",
+                "hostPort" : "3306"
+            }
+        ],
+        "environment" : [
           {
-              "containerPort" = "3306",
-              "hostPort" = "3306"
+            "name" : "MYSQL_ROOT_PASSWORD",
+            "value" : "test"
           }
-      ],
-      "environment" = [
-        {
-          "name" = "MYSQL_ROOT_PASSWORD",
-          "value" = "test"
-        }
-      ],
-      "log_configuration" = [{
-        "logDriver" = "awslogs",
-        "options" = {
-          "awslogs-create-group" = "true",
-          "awslogs-group" = "fargate-logs-test",
-          "awslogs-region" = "ap-southeast-1",
-          "awslogs-stream-prefix" = "ecs",
-        }
-      }]
-    }
-]
-DEFINITION
+        ],
+        "log_configuration" : [{
+          "logDriver" : "awslogs",
+          "options" : {
+            "awslogs-create-group" : "true",
+            "awslogs-group" : "fargate-logs-test",
+            "awslogs-region" : "ap-southeast-1",
+            "awslogs-stream-prefix" : "ecs",
+          }
+        }]
+      }
+  ]
+  TASK_DEFINITION
 
 }
