590440d30be968bc2ae7cbde0db6f2e70744d153
alvinswx <alvinswx@maxis.com.my>
2024-04-11 16:00:22 +0800

    its logConfiguration

--- ECS/MySQL/task_definition.tf
+++ ECS/MySQL/task_definition.tf
@@ -7,33 +7,33 @@ resource "aws_ecs_task_definition" "task_mysql" {
   task_role_arn = "arn:aws:iam::240835895323:role/ecsTaskExecutionRole"
   execution_role_arn = "arn:aws:iam::240835895323:role/ecsTaskExecutionRole"
   container_definitions = <<DEFINITION
-[
-  {
-    "name": "first-mysql",
-    "image": "mysql",
-    "essential": true,
-    "portMappings": [
-      {
-        "protocol": "tcp",
-        "containerPort": 3306,
-        "hostPort": 3306
-      }
-    ],
-    "environment": [
-      {
-        "name": "MYSQL_ROOT_PASSWORD",
-        "value": "testing"
-      }
-    ],
-    "logConfiguration": {
-      "logDriver": "awslogs",
-      "options": {
-        "awslogs-group": "/fargate/service/testing",
-        "awslogs-region": "ap-southeast-1",
-        "awslogs-stream-prefix": "ecs"
+  [
+    {
+      "name" : "first",
+      "image" : "mysql",
+      "essential" : true,
+      "portMappings" : [
+          {
+              "containerPort" : 3306,
+              "hostPort" : 3306
+          }
+      ],
+      "environment" : [
+        {
+          "name" : "MYSQL_ROOT_PASSWORD",
+          "value" : "test1"
+        }
+      ],
+      "logConfiguration" : {
+        "logDriver" : "awslogs",
+        "options" : {
+          "awslogs-group" : "fargate-logs-test",
+          "awslogs-region" : "ap-southeast-1",
+          "awslogs-stream-prefix" : "ecs"
+        }
       }
     }
-  }
-]
-DEFINITION
+  ]
+  DEFINITION
+
 }
