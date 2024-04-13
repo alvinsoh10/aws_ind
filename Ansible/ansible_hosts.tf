ba195c8a75c3140602851f7c2abf1b6cc7e85f0e
alvinswx <alvinswx@maxis.com.my>
2024-04-13 19:30:08 +0800

    qujotes for the variable

--- Ansible/ansible_hosts.tf
+++ Ansible/ansible_hosts.tf
@@ -1,7 +1,7 @@
 resource "local_file" "tf_ansible_hosts" {
   content = <<-DOC
     [mysql]
-    aws_instance.name.private_ip
+    ${aws_instance.name.private_ip}
   DOC
   filename = "./tf_ansible_hosts.yml"
 }
