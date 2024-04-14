b2e65d1a90ff6153dbb8a738a8c095920170b77e
alvinswx <alvinswx@maxis.com.my>
2024-04-14 17:21:53 +0800

    changed host file

--- Ansible/ansible_hosts.tf
+++ Ansible/ansible_hosts.tf
@@ -4,7 +4,6 @@ resource "local_file" "tf_ansible_hosts" {
       hosts:
         ${aws_instance.name.private_ip}:
           ansible_user: ubuntu
-          ansible_ssh_private_key_file: /var/lib/jenkins/workspace/ansible/Ansible/alvin-keypair.pem
   DOC
   filename = "./tf_ansible_hosts.yml"
 }
