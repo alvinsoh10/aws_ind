645c671131f2d018c1b32486cce188c60ff2b1d3
alvinswx <alvinswx@maxis.com.my>
2024-04-14 16:08:12 +0800

    edited host file location and keypair

--- Ansible/ansible_hosts.tf
+++ Ansible/ansible_hosts.tf
@@ -1,7 +1,10 @@
 resource "local_file" "tf_ansible_hosts" {
   content = <<-DOC
-    [mysql]
-    ${aws_instance.name.private_ip}
+    mysql:
+      hosts:
+        ${aws_instance.name.private_ip}:
+          ansible_user: ubuntu
+          ansible_ssh_private_key_file: /var/lib/jenkins/workspace/ansible/Ansible/alvin-keypair.pem
   DOC
   filename = "./tf_ansible_hosts.yml"
 }
